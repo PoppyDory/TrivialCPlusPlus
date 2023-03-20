51b8334cc158ea9459b64cb08181384d753ddac8
ma018ma <marzia018@libero.it>
2023-03-20 10:40:54 +0100

    numerate riposte

--- bozzatrivial.cpp
+++ bozzatrivial.cpp
@@ -5,10 +5,9 @@
 using namespace std;
 int main()
 {
-    ifstream fileQuestAnswer("domandeRisposte.txt"); // apro file di tutte domande risposte
-    ifstream fileRisposteGiuste("risposteEsatte.txt");// apro file di tutte risposte giuste
-    ofstream fileGiaUscite("domandeGiaUscite.txt");// volevo stampare su file le domande gia uscite ma penso non serva
-    if ((fileQuestAnswer.is_open()) && (fileRisposteGiuste.is_open())) //sono aperti domande e risposte e risposte giuste
+    ifstream fileQuestAnswer("domandeRisposte.txt");                   // apro file di tutte domande risposte
+    ifstream fileRisposteGiuste("risposteEsatte.txt");                 // apro file di tutte risposte giuste
+    if ((fileQuestAnswer.is_open()) && (fileRisposteGiuste.is_open())) // sono aperti domande e risposte e risposte giuste
     {
         cout << "fileOk" << endl;
     }
@@ -16,46 +15,51 @@ int main()
     {
         cout << "noFile" << endl;
     }
-    srand(time(NULL)); // random sull orario
-    bool giaUscite[30]; //vero e falso su indice di domanda gia uscite
+    srand(time(NULL));  // random sull orario
+    bool giaUscite[30]; // vero e falso su indice di domanda gia uscite
     for (int y = 0; y < 30; y++)
     {
         giaUscite[y] == false; // tutto l'arrey è inizialmente falso diventa vero più giu
     }
     int indiceDomanda = 0;
     int rigaDomanda = 0;
-    indiceDomanda = rand() % 30; 
-    //rigaDomanda = indiceDomanda * 5; 
-    while (giaUscite[indiceDomanda] == true) //solo se vero
+    do
     {
-        indiceDomanda = rand() % 30; //random su 30 numeri
-        rigaDomanda = indiceDomanda * 5;// moltiplica random trovato per 5 
-        cout << indiceDomanda << endl;
-        cout << rigaDomanda << endl;
-    }
+        indiceDomanda = rand() % 30;
+    } 
+    while (giaUscite[indiceDomanda] == true); // solo se vero
+
     cout << indiceDomanda << endl;
+    rigaDomanda = indiceDomanda *5;
     cout << rigaDomanda << endl;
-    string questAnswer; // file stampato su stringa 
-    string risposteGiuste; 
+
+    string questAnswer; // file stampato su stringa
+    string risposteGiuste;
+    int numeroRisposta =0;
     int contatore = 0; // contatore su righe da stampare
-    //while(getline(fileRisposteGiuste,risposteGiuste)) volevo far scorrere le risposte giuste
-    // bisogna inizializzare o un int o una string per risposta giusta
+    // while(getline(fileRisposteGiuste,risposteGiuste)) volevo far scorrere le risposte giuste
+    //  bisogna inizializzare o un int o una string per risposta giusta
     while (getline(fileQuestAnswer, questAnswer))
     {
-        contatore++; // contatore che aumenta
-        for(int i=1;i<6;i++){
-        if (contatore == rigaDomanda + i){ //se contatore trova l indice stampa da indice fino a 5
-            cout<< questAnswer << endl;
+        contatore++;
+            if (contatore == rigaDomanda +1)
+            { 
+                cout <<questAnswer << endl;
+            }
+        for (int i = 1; i < 5; i++)
+        {
+            if (contatore == (rigaDomanda +1) + i)
+            { 
+                cout << i <<"." <<questAnswer << endl;
             }
         }
-       // cout<< "scrivi as-aa-11 per risposta giusta" << endl;
-       // cin >> risposta << endl;//risposta - lettera - numero
-      // while(getline(fileRisposteGiuste,risposteGiuste))
-       //{
-        //if(risposta == risposteGiuste){
-           // cout<< "giusto!" << endl;
-       // }
-      // }
-    }        
+    }    //cout<< "scrivi numero per risposta giusta da 1 a 4" << endl;
+        //cin >> numeroRisposta ;
+        // while(getline(fileRisposteGiuste,risposteGiuste))
+        //{
+        // if(risposta == risposteGiuste){
+        // cout<< "giusto!" << endl;
+        // }
+        // }
 }
 
