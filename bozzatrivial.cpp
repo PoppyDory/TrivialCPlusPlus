c92e71f2775f148e48b544f135d49058d60d3e21
PoppyDory <lanza.rosita@gmail.com>
2023-03-20 15:50:47 +0100

    modifiche

--- bozzatrivial.cpp
+++ bozzatrivial.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 int main()
 {
-    ifstream fileQuestAnswer("domandeRisposte.txt");                   // apro file di tutte domande risposte
-    ifstream fileRisposteGiuste("risposteGiuste.txt");                 // apro file di tutte risposte giuste
-    
+    ifstream fileQuestAnswer("domandeRisposte.txt");   // apro file di tutte domande risposte
+    ifstream fileRisposteGiuste("risposteGiuste.txt"); // apro file di tutte risposte giuste
+
     if ((fileQuestAnswer.is_open()) && (fileRisposteGiuste.is_open())) // sono aperti domande e risposte e risposte giuste
     {
         cout << "fileOk" << endl;
@@ -16,24 +16,24 @@ int main()
     {
         cout << "noFile" << endl;
     }
-    srand(time(NULL));  // random sull orarion
-    
-    bool domandeGiaChieste[30]; 
-    
+    srand(time(NULL)); // random sull orarion
+
+    bool domandeGiaChieste[30];
+
     for (int y = 0; y < 30; y++)
     {
-        domandeGiaChieste[y] = false; // tutto l'arrey è inizialmente falso, diventa vero quando faccio una domanda 
+        domandeGiaChieste[y] = false; // tutto l'arrey è inizialmente falso, diventa vero quando faccio una domanda
     }
-    
+
     int indiceDomanda = 0;
     int rigaDomanda = 0;
-    
+
     do
     {
         indiceDomanda = rand() % 30;
     } while (domandeGiaChieste[indiceDomanda] == true); // solo se vero
 
-    rigaDomanda = indiceDomanda * 5 +1;
+    rigaDomanda = indiceDomanda * 5 + 1;
 
     string questAnswer; // file stampato su stringa
 
@@ -42,8 +42,8 @@ int main()
     // while(getline(fileRisposteGiuste,risposteGiuste)) volevo far scorrere le risposte giuste
     //  bisogna inizializzare o un int o una string per risposta giusta
     cout << endl;
-    cout<< "Domanda numero "<< indiceDomanda + 1 <<endl;
-    cout<<" ------------------"<<endl;
+    cout << "Domanda numero " << indiceDomanda + 1 << endl;
+    cout << " ------------------" << endl;
     while (getline(fileQuestAnswer, questAnswer))
     {
         contatore++;
@@ -52,7 +52,7 @@ int main()
             cout << questAnswer << endl;
             cout << endl; // stampo lariga della domanda
         }
-    
+
         for (int i = 1; i < 5; i++)
         {
             if (contatore == rigaDomanda + i)
@@ -62,29 +62,27 @@ int main()
             }
         }
     }
-    cout<< endl;
+    cout << endl;
     cout << "Seleziona la risposta giusta da 1 a 4: " << endl;
     int numeroRisposta = 0;
     cin >> numeroRisposta;
-    
+
     string risposteGiuste;
-    int trovaRiga = 0;
+    int trovaRiga = 0; // conta le righe fino a indice domanda
     bool seGiusto = false;
     string correzione = " ";
-    
+
     while (getline(fileRisposteGiuste, risposteGiuste))
     {
         // cout << numeroRisposta <<endl ;
         trovaRiga++;
-        
-        if ((trovaRiga == indiceDomanda+1) && (risposte[numeroRisposta] == risposteGiuste))
+        if ((trovaRiga == indiceDomanda + 1) && (risposte[numeroRisposta] == risposteGiuste))
         {
             seGiusto = true;
-            cout<< risposteGiuste << endl;
-
-            
+            cout << risposteGiuste << endl;
         } // confrontare stringa di risposteGiuste con filedomande
     }
+
     if (seGiusto == true)
     {
         cout << "La risposta e' GIUSTA" << endl;
@@ -94,15 +92,12 @@ int main()
         cout << "Purtoppo e' sbagliato" << endl;
         cout << "La risposta corretta e': " << correzione << endl;
     }
-    while (getline(fileRisposteGiuste, risposteGiuste)){
-                trovaRiga++;
-                if (trovaRiga == indiceDomanda+1)
+
+    while (getline(fileRisposteGiuste, risposteGiuste))
+    {
+        if (indiceDomanda + 1)
         {
-
-            cout<< risposteGiuste << endl;
-
-            
-        } 
-
+            cout << risposteGiuste << endl;
+        }
     }
 }
